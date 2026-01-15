f60e58f63455e775e11cf3defe3df10b83fff2eb
saleh siam <abusalehmdsiam@gmail.com>
2026-01-16 01:34:41 +0600

    Navbar and Topbar added

--- src/components/home/HomeClient.tsx
+++ src/components/home/HomeClient.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import Navbar from "../Navbar";
+import TopBar from "../TopBar";
+
 const HomeClient = () => {
   return (
     <div>
+      <TopBar/>
+      <Navbar/>
       <h1>Welcome to the Home Client Component</h1>
     </div>
   );
