123e2ee9dcc472348aa8e568edb3a8f17017f213
saleh siam <abusalehmdsiam@gmail.com>
2026-01-16 14:15:27 +0600

    navbar updated

--- src/components/home/HomeClient.tsx
+++ src/components/home/HomeClient.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import IntroSection from "../IntroSection";
-import Navbar from "../Navbar";
-import TopBar from "../TopBar";
 import TrustBadges from "../TrustBadges";
 
 const HomeClient = () => {
   return (
     <div>
-      <TopBar/>
-      <Navbar/>
       <TrustBadges/>
       <IntroSection/>
     </div>
